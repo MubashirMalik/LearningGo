bab0dc1fd63507eccb1c2fd39fa9df804efaea9f
mubashir <imubasherahmad@gmail.com>
2022-06-16 00:29:28 +0500

    add exercises for level-2

---
+++ level-2/exercise-3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	typed   int = 10
+	untyped     = 10
+)
+
+func main() {
+	fmt.Println(typed)
+	fmt.Println(untyped)
+}
---
+++ level-2/exercise-5/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := `this is a 
+	raw string 
+	literal
+	"inside a quotation"
+	`
+	fmt.Println(s)
+}
